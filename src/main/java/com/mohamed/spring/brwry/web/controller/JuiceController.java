c2ce520119df5689e4d07d38e2348b5d779afed3
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-06-20 15:55:56 -0400

    added get route

--- src/main/java/com/mohamed/spring/brwry/web/controller/JuiceController.java
+++ src/main/java/com/mohamed/spring/brwry/web/controller/JuiceController.java
@@ -1,4 +1,36 @@
 package com.mohamed.spring.brwry.web.controller;
 
+import com.mohamed.spring.brwry.services.JuiceService;
+import com.mohamed.spring.brwry.web.model.JuiceDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/juice")
+@RestController
 public class JuiceController {
+
+    private final JuiceService juiceService;
+
+    // dependency injection
+    public JuiceController(JuiceService juiceService) {
+        this.juiceService = juiceService;
+    }
+
+    @GetMapping("/{juiceId}")
+    // ResponseEntity gives us more control of what we return back
+    public ResponseEntity<JuiceDTO> getJuice(@PathVariable("juiceId") UUID juiceId) {
+        return new ResponseEntity<>(juiceService.getJuiceById(juiceId), HttpStatus.OK);
+
+
+    }
+
+
+
+
 }
