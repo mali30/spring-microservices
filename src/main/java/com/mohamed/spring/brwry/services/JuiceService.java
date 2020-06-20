9a9b659a78935c03f293783ba0619afab0820300
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-06-20 15:56:51 -0400

    added interface

--- src/main/java/com/mohamed/spring/brwry/services/JuiceService.java
+++ src/main/java/com/mohamed/spring/brwry/services/JuiceService.java
@@ -1,4 +1,9 @@
 package com.mohamed.spring.brwry.services;
 
+import com.mohamed.spring.brwry.web.model.JuiceDTO;
+
+import java.util.UUID;
+
 public interface JuiceService {
+    JuiceDTO getJuiceById(UUID juiceId);
 }
